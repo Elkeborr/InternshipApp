dfee59da855b1dbfc75c8ff0c1b4f9e9ca9c4719
robinvanbuggenhout <robinvanbuggenhout@icloud.com>
2019-10-03 18:01:27 +0200

    added logout btn

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,5 +19,14 @@
             </div>
         </div>
     </div>
+    <a class="dropdown-item" href="{{ route('logout') }}"	
+                                       onclick="event.preventDefault();	
+                                                     document.getElementById('logout-form').submit();">	
+                                        {{ __('Logout') }}	
+                                    </a>	
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">	
+                                        @csrf	
+                                    </form>
 </div>
 @endsection
