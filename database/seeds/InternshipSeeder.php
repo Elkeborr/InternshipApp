db8e89bab81c0f314d741ca78adbe6720403f3c8
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-10-10 20:17:00 +0200

    made factory for internship
    made a lot of dummy data ;)

--- database/seeds/InternshipSeeder.php
+++ database/seeds/InternshipSeeder.php
@@ -9,12 +9,14 @@ class InternshipSeeder extends Seeder
      */
     public function run()
     {
-        $internship = new \App\Internship();
-        $internship->company_name = 'Cronos';
-        $internship->company_city = 'Antwerpen';
-        $internship->internship_function = 'Back End Developer';
-        $internship->internship_discription = 'Lorem ipsum';
-        $internship->available_spots = '2';
-        $internship->save();
+        // $internship = new \App\Internship();
+        // $internship->company_name = 'Cronos';
+        // $internship->company_city = 'Antwerpen';
+        // $internship->internship_function = 'Back End Developer';
+        // $internship->internship_discription = 'Lorem ipsum';
+        // $internship->available_spots = '2';
+        // $internship->save();
+
+        factory(\App\Internship::class, 50)->create();
     }
 }
