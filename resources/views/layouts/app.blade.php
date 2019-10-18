429f4ba27e515f4db75f6366b2260421def6e268
Elkeborr <elkeborreij@student.thomasmore.be>
2019-10-18 15:05:33 +0200

    minor_sass_changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,15 +18,21 @@
 <body>
     <div id="app">
 <div class="top">
-<nav class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand" href="#">
+<nav class="navbar navbar-expand-md navbar-light">
+    <div class="d-flex w-50 order-">
+    <a class="navbar-brand navbar-brand mr-1" href="#">
         <img src="{{ asset('img/logo.png') }}" alt="Logo" height="30">
     </a>
-  <div class="navbar-nav navbar-center">
-      <a class="nav-item nav-link active" href="{{ url('/home') }}">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="{{ url('/') }}">Internships</a>
-      <a class="nav-item nav-link" href="{{ url('/companies') }}">Companies</a> 
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+</div>
+    <div class="collapse navbar-collapse justify-content-center order-2" id="collapsibleNavbar">
+        <a class="nav-link active nav-item " href="{{ url('/home') }}">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item  nav-link" href="{{ url('/') }}">Internships</a>
+        <a class="nav-item  nav-link" href="{{ url('/companies') }}">Companies</a> 
     </div>
+    <span class="navbar-text mt-1 w-50 text-right order-md-last"><a href="">username</a></span>
 </nav>
 </div>
         <main class="container">
