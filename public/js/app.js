aeeaeee7f40cd43321b7f4879f9cf00d642d6b60
Elkeborr <elkeborreij@student.thomasmore.be>
2019-10-18 15:07:15 +0200

    layout_changes

--- public/js/app.js
+++ public/js/app.js
@@ -49500,15 +49500,39 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/form.scss":
+/*!**********************************!*\
+  !*** ./resources/sass/form.scss ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open '/Users/ElkeBorreij/Documents/GitHub/InternshipApp/resources/sass/form.scss'\n    at runLoaders (/Users/ElkeBorreij/Documents/GitHub/InternshipApp/node_modules/webpack/lib/NormalModule.js:316:20)\n    at /Users/ElkeBorreij/Documents/GitHub/InternshipApp/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/ElkeBorreij/Documents/GitHub/InternshipApp/node_modules/loader-runner/lib/LoaderRunner.js:203:19\n    at process.nextTick (/Users/ElkeBorreij/Documents/GitHub/InternshipApp/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:73:15)\n    at process._tickCallback (internal/process/next_tick.js:61:11)");
+
+/***/ }),
+
+/***/ "./resources/sass/welcome.scss":
+/*!*************************************!*\
+  !*** ./resources/sass/welcome.scss ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open '/Users/ElkeBorreij/Documents/GitHub/InternshipApp/resources/sass/welcome.scss'\n    at runLoaders (/Users/ElkeBorreij/Documents/GitHub/InternshipApp/node_modules/webpack/lib/NormalModule.js:316:20)\n    at /Users/ElkeBorreij/Documents/GitHub/InternshipApp/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/ElkeBorreij/Documents/GitHub/InternshipApp/node_modules/loader-runner/lib/LoaderRunner.js:203:19\n    at process.nextTick (/Users/ElkeBorreij/Documents/GitHub/InternshipApp/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:73:15)\n    at process._tickCallback (internal/process/next_tick.js:61:11)");
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!**********************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/welcome.scss ./resources/sass/form.scss ***!
+  \**********************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/code/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/code/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/ElkeBorreij/Documents/GitHub/InternshipApp/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /Users/ElkeBorreij/Documents/GitHub/InternshipApp/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/ElkeBorreij/Documents/GitHub/InternshipApp/resources/sass/welcome.scss */"./resources/sass/welcome.scss");
+module.exports = __webpack_require__(/*! /Users/ElkeBorreij/Documents/GitHub/InternshipApp/resources/sass/form.scss */"./resources/sass/form.scss");
 
 
 /***/ })
