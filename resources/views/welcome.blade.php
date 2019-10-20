ec7e888c6ea23039ca1ebad0fbc5f16bcfeed65f
Elkeborr <elkeborreij@student.thomasmore.be>
2019-10-20 23:12:39 +0200

    layout_welcome_start

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,20 +10,65 @@
         <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
     </head>
     <body>
-        <div class="logo">
-            <img src="{{ asset('img/logo.png') }}" alt="Logo">
-        </div>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                    <h1>Welcome to sprintern</h1>
-                    <p>Are you a company or a student?</p>
-                </div>
+        <div class="hero">
+        <!--- Navigatie --->
+        <nav class="navbar navbar-expand-md navbar-light">
+            <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand ">
+                        <img src="{{ asset('img/logo.png') }}" alt="Logo" height="30">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="nav navbar-nav navbar-righ" id="collapsibleNavbar">
+                <a class="nav-link active nav-item " >About</a>
+                <a class="nav-item  nav-link" >Services</a>
+            </div>
+            </div>
+</nav>
+
+<!---Top--->
+
+            <div class="top">
+                <h1>Your next
+                    <span>intern/intership </span>
+                        is here</h1>
+                <p>Looking for an intern or an internship then signup on sprintern.<span> New opportunities everyday!</span></p>
+                <p class="top_question">Are you a company or a student?</p>
                 <div class="links">
                     <a href="{{ url('/companies/login') }}" class="company">Company</a>
                     <a href="{{ url('/redirect') }}" class="student">Student</a>
                 </div>      
             </div>
-        </div>
+</div>
+<!---About--->
+
+<div class="about">
+    <div></div>
+    <div> <h2>About us</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elit sem, molestie id rhoncus sit amet, accumsan facilisis lacus. Nulla vitae lacus interdum, consequat enim eget, euismod sapien. In lectus nisi, accumsan at dolor nec, elementum interdum dolor. Nulla vestibulum odio ipsum, vel vestibulum sem ultricies ut. </p>
+                </div>   
+</div>
+
+<!---Services--->
+
+<div class="services">
+                <h2>Services</h2>
+                <div class="services_content">
+                    <div class="service_1"> 
+                        <h3>For Companies</h3>
+                        <p>Companies can search for students who apply for their internships. ...</p>
+                    </div>
+                    <div class="service_2">
+                        <h3>For Students</h3>
+                        <p>Companies can search for students who apply for their internships. ...</p>
+                    </div>
+</div>
+              
+                 
+</div>
+
     </body>
 </html>
