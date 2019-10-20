8972c9ee311db0170b6c779a819834c6893ba725
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-10-21 01:49:32 +0200

    corrected mistake in UsersSeeder

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -11,7 +11,7 @@ class UsersSeeder extends Seeder
     {
         $user = new \App\User();
         $user->name = 'Wesley';
-        $user->email = 'Lorem ipsum';
+        $user->email = 'weske@company.com';
         $user->password = bcrypt('password');
         $user->type = 'company';
         $user->company_id = '1';
@@ -19,14 +19,14 @@ class UsersSeeder extends Seeder
 
         $user2 = new \App\User();
         $user2->name = 'Elke';
-        $user2->email = 'Sit dolor';
+        $user2->email = 'elke@student.com';
         $user2->password = bcrypt('password');
         $user2->type = 'student';
         $user2->save();
 
         $user3 = new \App\User();
         $user3->name = 'Robin';
-        $user3->email = 'Amet nogiets';
+        $user3->email = 'robin@company.com';
         $user3->password = bcrypt('password');
         $user3->type = 'company';
         $user3->company_id = '2';
@@ -34,7 +34,7 @@ class UsersSeeder extends Seeder
 
         $user4 = new \App\User();
         $user4->name = 'Angelique';
-        $user4->email = 'Zoveel Lorem ken ik niet!';
+        $user4->email = 'angelique@student.com';
         $user4->password = bcrypt('password');
         $user4->type = 'student';
         $user4->save();
