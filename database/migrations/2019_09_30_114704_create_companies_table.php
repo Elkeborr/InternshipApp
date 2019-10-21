8af4b6568d0d2d39dacfb30b68cab0c29fd75dd5
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-10-21 17:30:45 +0200

    added missing field in companies table

--- database/migrations/2019_09_30_114704_create_companies_table.php
+++ database/migrations/2019_09_30_114704_create_companies_table.php
@@ -13,6 +13,7 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
             $table->text('bio')->nullable();
             $table->string('phoneNumber');
             $table->string('email')->unique();
