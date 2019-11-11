d481eb7734ed31a7425b5c4c841c37397c44b865
Angelique <angeliquebuijzen@hotmail.com>
2019-11-11 12:39:00 +0100

    profile styling

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -11,9 +11,9 @@
 @section('content')
 <div class="row container-profile align-items-start">
     
-        <div class="col-md-12 profile text-center bg-light p-40">
+        <div class="col-md profile text-center bg-light p-40">
 
-                <img src="{{ asset('img/defaultProfile.png') }}" width="300" alt="profile picture">
+                <img src="{{ asset('img/profile-standard.jpg') }}" alt="profile picture" class="profilepic">
            
                 <h4>{{$user->name}}</h4>
 
@@ -23,7 +23,7 @@
                 
         </div>
                
-        <div class="col-md-12 profile">
+        <div class="col-md profile">
                 
         <div class="card-body button">       
         <button type="button" class="btn btn-dark text-right" onclick="window.location.href='/students/{{$user->id }}/edit'">Edit</button>
