29692b398e625dbe35854381aed0678b5b366b1c
Elkeborr <elkeborreij@student.thomasmore.be>
2019-11-15 15:20:49 +0100

    minor_nav_change

--- resources/views/layouts/detail.blade.php
+++ resources/views/layouts/detail.blade.php
@@ -19,21 +19,42 @@
     <div id="app">
 <div class="top">
 <nav class="navbar navbar-expand-md navbar-light">
-    <div class="d-flex w-50 order-">
-    <a class="navbar-brand navbar-brand mr-1" href="{{ url('/home') }}">
-        <img src="{{ asset('img/logo.png') }}" alt="Logo" height="30">
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-</div>
-    <div class="collapse navbar-collapse justify-content-center order-2" id="collapsibleNavbar">
-        <a class="nav-link active nav-item " href="{{ url('/home') }}">Home <span class="sr-only">(current)</span></a>
-        <a class="nav-item  nav-link" href="{{ url('/internships') }}">Internships</a>
-        <a class="nav-item  nav-link" href="{{ url('/companies') }}">Companies</a> 
-    </div>
-    <span class="navbar-text mt-1 w-50 text-right order-md-last"><a href="">{{$user->name}}</a></span>
-</nav>
+                @if($user = session('user'))
+                    <div class="d-flex w-50 order-">
+                        <a class="navbar-brand navbar-brand mr-1" href="{{ url('/home') }}">
+                            <img src="{{ asset('img/logo.png') }}" alt="Logo" height="30">
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
+           
+                        @if($user->type == 'company')
+                            <!-- <p>{{$user->name}}</p> -->
+                            <a class="nav-link active nav-item " href="{{ url('/home') }}">Home <span class="sr-only">(current)</span></a>
+                            <a class="nav-item  nav-link" href="{{ url('/internships') }}">Interns</a>
+                            <a class="nav-item  nav-link" href="{{ url('/companies/myinternships') }}">My Internships</a> 
+                        @endif
+                        @if($user->type == 'student')
+                            <!-- <p>{{$user->name}}</p> -->
+                            <a class="nav-link active nav-item " href="{{ url('/home') }}">Home <span class="sr-only">(current)</span></a>
+                            <a class="nav-item  nav-link" href="{{ url('/internships') }}">Internships</a>
+                            <a class="nav-item  nav-link" href="{{ url('/companies') }}">Companies</a>  
+                        @endif
+        
+                    </div>
+
+                    <span class="navbar-text mt-1 w-50 text-right order-md-last username">
+                    
+                    <!-- @if ($flash = session('username')) -->
+                        <a href="{{ url('/users/detail') }}">{{$flash}}</a>
+                    <!-- @endif -->
+                    <span class="navbar-text mt-1 w-50 text-right order-md-last"><a href="/students/{{$user->id }}">{{$user->name}}</a></span>
+                    </span>
+
+                @endif
+            </nav>
 
 </div>
         <main class="container">
