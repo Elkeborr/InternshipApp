4d1d4a199b7deb6ed44d16a12e24bbac3d74f07a
Elkeborr <elkeborreij@student.thomasmore.be>
2019-11-22 13:29:15 +0100

    Translated company details to dutch

--- resources/views/companies/detail.blade.php
+++ resources/views/companies/detail.blade.php
@@ -13,8 +13,10 @@
   <!-- content -->
 
 
-<h2>Company details</h2>
-<p>Before you start your search for interns you have to complete this form so that your account is complete.</p>
+<h2>Bedrijf details</h2>
+<p>Voor je verder kan gaan moet je nog een paar details invullen over je bedrijf. We hebben al enkele 
+  gegevens gevonden voor jou. 
+</p>
           <form method="post" action="">
           <div class="row no-gutters">
   <div class="col-6"> 
@@ -28,11 +30,11 @@
             @endif
             <div class="form-row">
               <div class="form-group col-md-6">
-                <label for="inputName">Name</label>
+                <label for="inputName">Naam</label>
                 @if (isset($company['name']))
                 <input type="name" class="form-control" id="inputName" value="{{ $company['name']}}" name="name" required>
                 @else
-                <input type="name" class="form-control" id="inputName"placeholder="name" name="name" required>
+                <input type="name" class="form-control" id="inputName"placeholder="naam" name="name" required>
                 @endif
               </div>
             </div>
@@ -45,46 +47,46 @@
   </div>
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputPhonenumber">Phonenumber</label>
-      <input type="phoneNumber" class="form-control" id="inputPhonenumber" placeholder="phonenumber" name="phoneNumber" required>
+      <label for="inputPhonenumber">Telefoon</label>
+      <input type="phoneNumber" class="form-control" id="inputPhonenumber" placeholder="telefoon" name="phoneNumber" required>
     </div>
   </div>
 
   <div class="form-row">
     <div class="form-group col-md-3">
-      <label for="inputStreet">Street </label>
+      <label for="inputStreet">Straat</label>
       @if (isset($company['location']['address']))
       <input type="text" class="form-control" id="inputStreet"  value="{{$company['location']['address']}}" name="street" required>
       @else
-      <input type="text" class="form-control" id="inputStreet" placeholder="street" name="street" required>
+      <input type="text" class="form-control" id="inputStreet" placeholder="straat" name="street" required>
      @endif
     </div>
     <div class="form-group col-md-3">
-      <label for="inputStreetNumber">Street number</label>
-      <input type="text" class="form-control" id="inputStreetNumber" placeholder="street number"name="streetNumber" required>
+      <label for="inputStreetNumber">Huisnummer</label>
+      <input type="text" class="form-control" id="inputStreetNumber" placeholder="huisnummer"name="streetNumber" required>
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
+      <label for="inputCity">Stad</label>
       @if (isset($company['location']['city']))
       <input type="text" class="form-control" id="inputStreet"  value="{{$company['location']['city']}}" name="city" required>
       @else
-      <input type="text" class="form-control" id="inputStreet" placeholder="city" name="city" required>
+      <input type="text" class="form-control" id="inputStreet" placeholder="stad" name="city" required>
      @endif
     </div>
   </div>
   <div class="form-row">
   <div class="form-group col-md-3">
-      <label for="inputZip">Postal code</label>
-      <input type="text" class="form-control" id="inputZip" placeholder="postal code" name="postalCode" required>
+      <label for="inputZip">Postcode</label>
+      <input type="text" class="form-control" id="inputZip" placeholder="postcode" name="postalCode" required>
     </div>
     <div class="form-group col-md-3">
-      <label for="inputState">State</label>
+      <label for="inputState">Provincie</label>
       @if (isset($company['location']['state']))
       <input type="text" class="form-control" id="inputState"  value="{{$company['location']['state']}}" name="state" required>
       @else
-      <input type="text" class="form-control" id="inputState" placeholder="state"  name="state" required>
+      <input type="text" class="form-control" id="inputState" placeholder="provincie"  name="state" required>
      @endif
     </div>
     </div>
@@ -93,21 +95,21 @@
 <div class="col-6"> 
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputEmployees">Employees</label>
-      <input type="number" class="form-control" id="inputEmployees" placeholder="Employees"  name="employees" required>
+      <label for="inputEmployees">Werknemers</label>
+      <input type="number" class="form-control" id="inputEmployees" placeholder="werknemers"  name="employees" required>
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="inputBio">Bio</label>
-      <textarea rows="4" cols="50" type="text" class="form-control" id="inputBio" placeholder="Tell us about your company"  name="bio" required>
+      <textarea rows="4" cols="50" type="text" class="form-control" id="inputBio" placeholder="Vertel ons meer over je bedrijf"  name="bio" required>
       </textarea>
     </div>
   </div>
 
   <div class="form-row">
     <div class="form-group col-md-6 tags" >
-      <label for="inputTags">Tags</label> <br>
+      <label for="inputTags">Categorie</label> <br>
       @foreach($tags?? '' as $tag)
       <input type="checkbox" name="tag[]" value="{{ $tag-> id}}"> {{ $tag-> name}}<br>
       @endforeach
