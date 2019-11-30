e8c1cc7959db22f273e72e0a6cc84f9b97cd3d72
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-11-30 17:50:07 +0100

    Fixing small mistakes made by merging

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,58 +9,44 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 /* Welcome page */
 Route::get('/', 'InternshipController@welcomeIndex');
-
 /* Login en registratie van studenten & bedrijven */
-
 /* Login */
 Route::get('/companies/login', 'CompanyController@login');
 Route::post('/companies/login', 'CompanyController@handleLogin');
 Route::get('/students/login', 'StudentController@login');
 Route::post('/students/login', 'StudentController@handleLogin');
-
 /* Register students */
 Route::get('/students/register', 'StudentController@register');
 Route::post('/students/register', 'StudentController@handleRegister');
-
 /* Register */
 Route::get('/companies/register', 'CompanyController@register');
 Route::post('/companies/register', 'CompanyController@handleRegister');
-
 // --------   enkel als je ingelogd bent kunnen deze routes ingeladen worden --------//
     Route::group(['middleware' => 'auth'], function () {
         /*COMPANIES*/
-
         /*Create company*/
         Route::get('/companies/detail', 'CompanyController@create');
         Route::post('/companies/detail', 'CompanyController@handlecreate');
-
         /* Companies */
         Route::get('/companies', 'CompanyController@index');
         Route::get('/companies/{company}', 'CompanyController@show');
-
         /*----------------STUDENTS ------------------------- */
-
         /* profielpagina */
         Route::get('/students', 'StudentController@index');
         Route::get('/students/{student}', 'StudentController@show');
-
         /* studentprofile edit-information-form */
         Route::get('/students/{student}/edit', 'StudentController@edit');
         Route::get('/students/{student}/edit-intro', 'StudentController@editIntro');
         Route::get('/students/{student}/edit-skills', 'StudentController@editSkills');
         Route::get('/students/{student}/edit-social', 'StudentController@editSocial');
-
         /* studentprofile add-information-form */
         Route::get('/students/{student}/add-skills', 'StudentController@addSkills');
         Route::get('/students/{student}/add-social', 'StudentController@addSocial');
-
         /* studentprofile update info */
         Route::put('/students/update/{student}', 'StudentController@update');
         Route::put('/students/updateIntro/{student}', 'StudentController@updateIntro');
@@ -69,38 +54,30 @@ Route::post('/companies/register', 'CompanyController@handleRegister');
         Route::put('/students/deleteSkills/{student}', 'StudentController@deleteSkills');
         Route::put('/students/updateSocial/{student}', 'StudentController@updateSocial');
         Route::put('/students/deleteSocial/{student}', 'StudentController@deleteSocial');
-
         /* studentprofile add info */
         Route::put('/students/addSkills/{student}', 'StudentController@saveSkills');
         Route::put('/students/addSocial/{student}', 'StudentController@saveSocial');
-
         Auth::routes();
-
         /* Internships */
         Route::get('/home', 'HomeController@index')->name('home');
-
         Route::get('/internships/myinternships', 'InternshipController@showMyInternships');
         Route::get('/internships/myinternships/create', 'InternshipController@create');
         Route::post('/internships/myinternships/create', 'InternshipController@handleCreate');
-
         Route::get('/internships/{internship}', 'InternshipController@show');
         Route::get('/internships', 'InternshipController@index');
-
         Route::group(['middleware' => 'auth'], function () {
             Route::get('/internships', 'InternshipController@index');
         });
-
         /* Apply */
         Route::get('/internships/{internship}/apply', 'JobApplicationController@apply');
-
         /* Company internships & applies */
         Route::get('/companies/myinternships/{internship}/applications', 'JobApplicationController@applications');
         Route::post('/{id}/save', 'JobApplicationController@save');
-
         /* Facebook login */
         Route::get('/redirect', 'SocialAuthFacebookController@redirect');
         Route::get('/callback', 'SocialAuthFacebookController@callback');
-        
+
         /* Search */
         // Route::get('/', 'SearchController@index');
         Route::post('/search', 'SearchController@search');
+    });
