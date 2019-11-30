643927e53ef30cf168677b70e6c52e7b75cec8c8
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-11-30 17:28:34 +0100

    fixed student login-page

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Auth;
+use Redirect;
 
 class StudentController extends Controller
 {
@@ -27,11 +29,7 @@ class StudentController extends Controller
     {
         $data['user'] = \App\User::where('id', $user)->first();
 
-        if ($user == \Auth::User()->id) {
-            return view('/students/edit', $data);
-        } else {
-            return view('/students/show', $data);
-        }
+        return view('/students/edit', $data);
     }
 
     public function editIntro($user)
@@ -41,11 +39,11 @@ class StudentController extends Controller
         return view('/students/edit-intro', $data);
     }
 
-    public function editKwaliteiten($user)
+    public function editSkills($user)
     {
         $data['user'] = \App\User::where('id', $user)->first();
 
-        return view('/students/edit-kwaliteiten', $data);
+        return view('/students/edit-skills', $data);
     }
 
     public function editSocial($user)
@@ -55,19 +53,19 @@ class StudentController extends Controller
         return view('/students/edit-social', $data);
     }
 
-    public function addKwaliteiten($user)
+    public function addSkills($user)
     {
         $data['user'] = \App\User::where('id', $user)->first();
 
-        return view('/students/add-kwaliteiten', $data);
+        return view('/students/add-skills', $data);
     }
 
-    public function saveKwaliteiten(Request $request)
+    public function saveSkills(Request $request)
     {
         $validation = $request->validate([
             'skill' => 'required',
         ]);
-
+        /* werkt alleen voor de laatste */
         $user = session('user');
         $skill = new \App\Skill();
         $skill->skill = request('skill');
@@ -75,6 +73,26 @@ class StudentController extends Controller
         $skill->save();
 
         return redirect()->action('StudentController@show', $user);
+    }
+
+    public function deleteSkills(Request $request)
+    {
+        $user = session('user');
+        $id = request('skillid');
+        $skill = \App\Skill::where('id', $id);
+        $skill->delete();
+
+        return redirect()->action('StudentController@editSkills', $user);
+    }
+
+    public function deleteSocial(Request $request)
+    {
+        $user = session('user');
+        $id = request('socialId');
+        $social = \App\Social::where('id', $id);
+        $social->delete();
+
+        return redirect()->action('StudentController@editSocial', $user);
     }
 
     public function addSocial($user)
@@ -90,7 +108,6 @@ class StudentController extends Controller
             'socialname' => 'required',
             'sociallink' => 'required|starts_with:http://',
         ]);
-
         $user = session('user');
         $social = new \App\Social();
         $social->link = request('sociallink');
@@ -108,9 +125,7 @@ class StudentController extends Controller
             'email' => 'required',
             'password' => 'required',
         ]);
-
         $user = session('user');
-
         $user->name = request('firstname');
         $user->lastname = request('lastname');
         $user->email = request('email');
@@ -130,21 +145,99 @@ class StudentController extends Controller
         return redirect()->action('StudentController@show', $user);
     }
 
-    public function updateKwaliteiten(Request $request)
+    public function updateSkills(Request $request)
     {
+        $validation = $request->validate([
+            'skill' => 'required',
+        ]);
         $user = session('user');
-        $skill = \App\Skill::where('id', request('skillid'));
+        $skill = \App\Skill::where('id', request('skillid'))->first();
         $skill->skill = request('skill');
+        $skill->user_id = $user->id;
         $skill->save();
-        $user = \Auth::User();
 
-        return redirect()->action('StudentController@show', $user->id);
+        return redirect()->action('StudentController@editSkills', $user);
     }
 
     public function updateSocial(Request $request)
     {
+        $validation = $request->validate([
+            'socialName' => 'required',
+            'socialLink' => 'required|starts_with:http://',
+        ]);
         $user = session('user');
+        $social = \App\Social::where('id', request('socialId'))->first();
+        $social->name = request('socialName');
+        $social->link = request('socialLink');
+        $social->user_id = $user->id;
+        $social->save();
 
-        return redirect()->action('StudentController@show', $user);
+        return redirect()->action('StudentController@editSocial', $user);
+    }
+
+    /* -------------------- LOGIN ----------------------- */
+    public function login()
+    {
+        return view('students/login');
+    }
+
+    public function handleLogin(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+        $request->flash();
+        if (Auth::attempt($credentials)) {
+            $request->session()->flash('message', 'Login successvol!');
+            //Retrieve data and put it in session
+            $user_id = Auth::id();
+            $user = \App\User::where('id', $user_id)->select('id', 'name', 'email', 'type', 'company_id')->first();
+            if ($user->type == 'student') {
+                //Put user data in session User
+                $request->session()->put('user', $user);
+                // dd($sessionData['name']);
+                return redirect('home');
+            }
+            $request->session()->flash('message', 'Hier kunnen enkel studenten inloggen');
+
+            return view('students/login');
+        }
+        $request->session()->flash('message', 'Login lukt niet, probeer opnieuw');
+
+        return view('studends/login');
+    }
+
+    public function register(Request $request)
+    {
+        return view('students/register');
+    }
+
+    public function handleRegister(Request $request)
+    {
+        $validation = $request->validate([
+              'firstName' => 'required',
+              'email' => ['unique:users,email'],
+              'password' => ['required', 'string', 'min:8', 'confirmed'],
+          ]);
+        $request->flash();
+        $user = new \App\User();
+        $user->name = $request->input('firstName');
+        $user->email = $request->input('email');
+        $user->password = \Hash::make($request->input('password'));
+        $user->type = 'student';
+        $user->save();
+        $credentials = $request->only(['email', 'password']);
+        if (Auth::attempt($credentials)) {
+            $request->session()->flash('username', $user->name);
+            $request->session()->flash('message', 'Studentregistratie succesvol!');
+            $request->session()->flash('email', $user->email);
+            //Retrieve data and put it in session
+            $user_id = Auth::id();
+            $user = \App\User::where('id', $user_id)->select('id', 'name', 'email', 'type', 'company_id')->first();
+            //Put user data in session User
+            $request->session()->put('user', $user);
+            // dd($sessionData['name']);
+            return view('/home');
+        }
+
+        return view('students/login');
     }
 }
