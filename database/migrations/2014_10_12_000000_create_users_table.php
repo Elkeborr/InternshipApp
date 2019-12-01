514e270cdad52d7690c8fa0449dc9ec62728ce24
vagrant <vagrant@internshipapp>
2019-12-01 22:30:35 +0000

    Also generated a uuid for companies

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,9 +12,6 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            //$table->string('id');
-            //$table->bigIncrements('id');
-            //$table->uuid('uuid');
             $table->uuid('id');
             $table->primary('id');
             $table->string('name');
