6a35624b2df7d591817070d8d69b59fdd3ff9004
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-12-02 20:28:48 +0100

    Company_id is not nullable anymore

--- database/migrations/2019_10_10_163300_create_internships_table.php
+++ database/migrations/2019_10_10_163300_create_internships_table.php
@@ -16,7 +16,7 @@ class CreateInternshipsTable extends Migration
             $table->string('internship_function')->nullable();
             $table->text('internship_discription')->nullable();
             $table->integer('available_spots');
-            $table->integer('company_id')->nullable();
+            $table->integer('company_id');
             $table->timestamps();
         });
     }
