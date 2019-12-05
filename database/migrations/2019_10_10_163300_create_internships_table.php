846ab486e1e8fdfdf6357ce69b423b1371bef280
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-12-05 13:57:38 +0100

    Fixed db migration and seed
    InternshipSeeder didnt have an comany_id and that company_id was still nullable

--- database/migrations/2019_10_10_163300_create_internships_table.php
+++ database/migrations/2019_10_10_163300_create_internships_table.php
@@ -16,7 +16,7 @@ class CreateInternshipsTable extends Migration
             $table->string('internship_function')->nullable();
             $table->text('internship_discription')->nullable();
             $table->integer('available_spots');
-            $table->bigInteger('company_id')->nullable();
+            $table->bigInteger('company_id');
             $table->timestamps();
         });
     }
