e958f9b1fea3f12fd7ff11aedd839ac44a52c1e6
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-12-11 23:01:03 +0100

    made validation for amount of employees of a company

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -137,6 +137,19 @@ class CompanyController extends Controller
 
     public function handlecreate(Request $request)
     {
+        // $this->validate($request, [
+        //     'name' => 'required',
+        //     'bio' => 'required',
+        //     'phoneNumber' => 'required',
+        //     'email' => 'required',
+        //     'street' => 'required',
+        //     'streetNumber' => 'required',
+        //     'city' => 'required',
+        //     'state' => 'required',
+        //     'postalCode' => 'required',
+        //     'employees' => 'required|gt:0',
+        // ]);
+
         $user = session('user');
         $company = new \App\Company();
 
