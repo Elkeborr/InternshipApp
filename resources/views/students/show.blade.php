f5c65ccafd136e18450d3776311f1b550410648a
Angelique <angeliquebuijzen@hotmail.com>
2019-12-12 23:30:04 +0100

    Update show.blade.php

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -30,6 +30,11 @@ Profiel
         <h5>Contact:</h5>
         <p>{{$user->email}}</p>
 
+        @if($user->type == 'company')
+            <h5>Bedrijfsgegevens</h5>
+            <p>{{$user->$company->phoneNumber}}</p>
+        @endif
+
     </div>
 
     <div class="col-md profile">
