1ebc527c0ecd33129b9e05a61151f511f5a9fe3e
robinvanbuggenhout <robinvanbuggenhout@icloud.com>
2019-12-13 14:57:00 +0100

    behoud oude waarden wanneer het social media veld fout is ingevuld

--- public/js/app.js
+++ public/js/app.js
@@ -49541,10 +49541,10 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/angeliquebuijzen/Documents/GitHub/InternshipApp/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /Users/angeliquebuijzen/Documents/GitHub/InternshipApp/resources/sass/app.scss */"./resources/sass/app.scss");
-__webpack_require__(/*! /Users/angeliquebuijzen/Documents/GitHub/InternshipApp/resources/sass/welcome.scss */"./resources/sass/welcome.scss");
-module.exports = __webpack_require__(/*! /Users/angeliquebuijzen/Documents/GitHub/InternshipApp/resources/sass/form.scss */"./resources/sass/form.scss");
+__webpack_require__(/*! /home/vagrant/code/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /home/vagrant/code/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/code/resources/sass/welcome.scss */"./resources/sass/welcome.scss");
+module.exports = __webpack_require__(/*! /home/vagrant/code/resources/sass/form.scss */"./resources/sass/form.scss");
 
 
 /***/ })
